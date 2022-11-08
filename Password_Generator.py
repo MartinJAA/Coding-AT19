8320f81a314093282a51759c7019d34c214e446e
MartinJAA <serverus15>
2022-11-08 18:01:22 -0400

    Try 2

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,1 +1,22 @@
-# this is a tried
+# password = chars > 8 and chars <= 16
+#           at least one capital letter
+#           at least one number
+#           at least one lower letter
+import random
+
+lower_letter = "abcdefghijklmnopqrstuvwxyz"
+capital_letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+
+len_password = random.randint(9, 16)            #chars
+index_capital_letter = random.randint(1,4)
+index_number = random.randint(5,8)
+
+for chars in range(len_password):
+    if index_capital_letter == chars:
+        print(random.choice(capital_letter), end='')
+    elif index_number == chars:
+        print(random.choice(number), end='')
+    else:
+        print(random.choice(lower_letter), end='')
+
