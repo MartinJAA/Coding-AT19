87791d62d488e4e1665d91ffb4884cf9d381497f
MartinJAA <serverus15>
2022-11-11 17:43:05 -0400

    Final version of password generator, everything works

--- Password_Generator.py
+++ Password_Generator.py
@@ -30,19 +30,26 @@ while True:
     if option == "1":
         account = input("Enter an account: ")
         my_dict[account] = give_me_password(random.randint(9, 16))
-        with open('password.txt', 'w') as data:
-            data.write(str(my_dict))
-        print(f'{account}: {my_dict.get(account)}')
+        # with open('password.txt', 'w') as data:
+        #     data.write(str(my_dict))
+        #     print(f'{account}: {my_dict.get(account)}')
+            # my_dict = {} no como estructura de datos sino como datos separados un for puede ser
+        with open("password.txt", "a") as data:
+            for k, v in my_dict.items():
+                data.write((str(k) + " " + str(v) + "\n"))
+                print(f'{account}: {my_dict.get(account)}')
 
     elif option == "2":
         account = input("Enter the name account: ")
+        # with open('password.txt') as data:
+        #     info = data.read()
+        #     my_dict2 = ast.literal_eval(info)
         with open('password.txt') as data:
-            info = data.read()
-            my_dict = ast.literal_eval(info)
-            if my_dict.get(account) is None:
+            info = dict(x.rstrip().split(None, 1) for x in data)
+            if info.get(account) is None:
                 print("Account no registered, please register it")
             else:
-                print(f"{account} : {my_dict.get(account)}")
+                print(f"{account} : {info.get(account)}")
     elif option == "3":
         print("Exiting...")
         break
