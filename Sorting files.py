7c2c1d8f3bb1d7e75f2801fa60c98754d63afd38
MartinJAA <serverus15>
2022-11-14 15:34:17 -0400

    Sorting files task

--- Sorting files.py
+++ Sorting files.py
@@ -13,3 +13,19 @@ def create_folder(list_name):
     return print("Folders created")
 
 
+def find_pdf(path):
+    pdf_list = []
+    read_files = os.listdir(path)
+    for items in read_files:
+        if items.endswith(".pdf"):
+            pdf_list.append(items)
+    print(pdf_list)
+
+
+def find_folders(path):
+    # folder_list = []
+    read_folders = next(os.walk('data'))[1]
+    return read_folders
+
+
+
